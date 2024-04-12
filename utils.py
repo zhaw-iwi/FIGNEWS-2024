b881c4cf890ee785363ef57b6de87ba1ed609306
JasminH <j.r.heierli@gmail.com>
2024-04-12 14:32:04 +0200

    change to openai

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from openai import AzureOpenAI
+from openai import OpenAI
 from dotenv import load_dotenv
 import os
 import logging
@@ -20,19 +20,31 @@ logger.addHandler(file_handler)
 # setup azureopenai credentials
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-OPENAI_DEPLOYMENT_ENDPOINT = os.getenv("OPENAI_DEPLOYMENT_ENDPOINT")
-OPENAI_DEPLOYMENT_NAME = os.getenv("OPENAI_DEPLOYMENT_NAME")
 OPENAI_MODEL_NAME = os.getenv("OPENAI_MODEL_NAME")
-OPENAI_DEPLOYMENT_VERSION = os.getenv("OPENAI_DEPLOYMENT_VERSION")
+
+
+def append_data(df, worksheet, number_of_cols):
+    values = worksheet.get_all_values()
+    col = colnumn_string(max([len(r) for r in values]) + 1)
+    worksheet.add_cols(number_of_cols)
+    worksheet.update(
+        values=df.values.tolist(),
+        range_name=col + "2",
+        value_input_option="USER_ENTERED",
+    )
+
+
+def colnumn_string(n):
+    string = ""
+    while n > 0:
+        n, remainder = divmod(n - 2, 26)
+        string = chr(65 + remainder) + string
+    return string
 
 
 def lm_completion(messages: list[dict]) -> str:
 
-    client = AzureOpenAI(
-        azure_endpoint=OPENAI_DEPLOYMENT_ENDPOINT,
-        api_key=OPENAI_API_KEY,
-        api_version=OPENAI_DEPLOYMENT_VERSION,
-    )
+    client = OpenAI(api_key=OPENAI_API_KEY)
 
     logger.info("Messages: %s", messages)
     start = time.perf_counter()
